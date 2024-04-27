cf87ae5bf5e88cf1ce0a0d4aaf848b6e5e866df7
yunusemreozdemir <emreozdemir@mail.com>
2024-04-27 19:07:51 +0300

    feat(frontend): add index to export pages

---
+++ frontend/src/pages/index.tsx
@@ -0,0 +1,6 @@
+export { default as Home } from './Home'
+export { default as Feed } from './Feed'
+export { default as Profile } from './Profile'
+export { default as Architect } from './Architect'
+export { default as Building } from './Building'
+export { default as Style } from './Style'
