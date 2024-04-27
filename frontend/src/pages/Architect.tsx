c7823828e16c972999f3d3b1ea52c06c84e9fd2f
yunusemreozdemir <emreozdemir@mail.com>
2024-04-27 19:10:31 +0300

    feat(frontend): initialize Architect page

---
+++ frontend/src/pages/Architect.tsx
@@ -0,0 +1,10 @@
+import { Navbar } from '../components'
+
+export default function Architect() {
+    return (
+        <div>
+            <Navbar />
+            <h1>Architect</h1>
+        </div>
+    )
+}
