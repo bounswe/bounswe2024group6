43fd910944cc507327b39c02e513d15d6f36d527
yunusemreozdemir <emreozdemir@mail.com>
2024-04-27 19:10:44 +0300

    feat(frontend): initialize Building page

---
+++ frontend/src/pages/Building.tsx
@@ -0,0 +1,10 @@
+import { Navbar } from '../components'
+
+export default function Building() {
+    return (
+        <div>
+            <Navbar />
+            <h1>Building</h1>
+        </div>
+    )
+}
