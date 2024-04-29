f6dcaf9230d0f667e41017685ba56de6d7414962
yunusemreozdemir <emreozdemir@mail.com>
2024-04-29 12:57:59 +0300

    feat(frontend): design initial Navbar component

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,16 +1,45 @@
 import { useAuth } from '../hooks'
+import { Input } from "@/components/ui/input"
+import BookmarksOutlinedIcon from '@mui/icons-material/BookmarksOutlined';
+import { Avatar, AvatarFallback, AvatarImage, } from "@/components/ui/avatar"
+import { Link } from "react-router-dom";
 
 export default function Navbar() {
     const { isAuth } = useAuth()
 
     return (
-        <div>
+        <div className="flex bg-zinc-900 text-white w-full items-center justify-between py-3 px-6">
+            <div className="flex items-center gap-3">
+                <div className="text-xl">
+                    searchitect
+                </div>
+                <div className="h-7 w-0.5 bg-white"></div>
+                <Link to="/feed">
+                    Feed
+                </Link>
+            </div>
+            <div className="flex justify-center w-1/4">
+                <Input placeholder="Search..." className="bg-zinc-900" />
+            </div>
             {
                 isAuth
                 ?
-                    <h1>Auth User Navbar</h1>
+                    <div className="flex items-center gap-3">
+                        <BookmarksOutlinedIcon />
+                        <Avatar className="border-white border-2">
+                            <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
+                            <AvatarFallback>CN</AvatarFallback>
+                        </Avatar>
+                    </div>
                 :
-                    <h1>Guest User Navbar</h1>
+                    <div className="flex items-center gap-2">
+                        <div>
+                            Login
+                        </div>
+                        <div>
+                            Register
+                        </div>
+                    </div>
             }
         </div>
     )
