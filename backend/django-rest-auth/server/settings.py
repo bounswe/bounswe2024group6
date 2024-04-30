e7790cf2395ddaacbb87848b29830656015f47b3
yunusemreozdemir <emreozdemir@mail.com>
2024-04-30 21:05:28 +0300

    fix(backend): add localhost to allowed hosts

--- backend/django-rest-auth/server/settings.py
+++ backend/django-rest-auth/server/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-q1-xt8(+yr^6iye@sa3@miqn&(#-be96ild1s!o)wlmwqrzd3-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["127.0.0.1"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
 
 
 # Application definition
