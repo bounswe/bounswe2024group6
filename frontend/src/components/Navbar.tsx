b27226804d4b556fc41d46a15a95091e4a475a3a
yunusemreozdemir <emreozdemir@mail.com>
2024-04-30 21:07:00 +0300

    feat(frontend): change Navbar search bar to form

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,8 +1,9 @@
 import { useAuth } from '../hooks'
-import { Input } from "@/components/ui/input"
 import BookmarksOutlinedIcon from '@mui/icons-material/BookmarksOutlined';
 import { Avatar, AvatarFallback, AvatarImage, } from "@/components/ui/avatar"
 import { Link, useNavigate } from "react-router-dom";
+import { useState } from 'react'
+import { useForm } from "react-hook-form"
 import {
     DropdownMenu,
     DropdownMenuContent,
@@ -11,13 +12,43 @@ import {
     DropdownMenuSeparator,
     DropdownMenuTrigger,
   } from "@/components/ui/dropdown-menu"
-  import { Button } from "@/components/ui/button"  
-  import SearchIcon from '@mui/icons-material/Search';
+import { Button } from "@/components/ui/button"
+import {
+Form,
+FormControl,
+FormDescription,
+FormField,
+FormItem,
+FormLabel,
+FormMessage,
+} from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
+import SearchIcon from '@mui/icons-material/Search';
+import { z } from "zod"
+import { zodResolver } from "@hookform/resolvers/zod"
+
+const searchSchema = z.object({
+    query: z.string().min(5).max(50),
+  })
+
 
 export default function Navbar() {
     const { checkAuth, logout } = useAuth()
     const isAuth = checkAuth()
     const navigate = useNavigate()
+    const [query, setQuery] = useState<string>("")
+
+    const searchForm = useForm<z.infer<typeof searchSchema>>({
+        resolver: zodResolver(searchSchema),
+        defaultValues: {
+            query: "",
+        },
+    })
+
+    function handleSearch(values: z.infer<typeof searchSchema>) {
+        navigate(`/wiki/browse?q=${values.query}`);
+        navigate(0);
+    }
 
     return (
         <div className="flex bg-zinc-900 text-white w-full items-center justify-between py-3 px-6">
@@ -30,10 +61,26 @@ export default function Navbar() {
                     Feed
                 </Link>
             </div>
-            <div className="flex justify-center gap-2 w-1/4">
-                <Input placeholder="Search..." className="bg-zinc-900" />
-                <Button variant="outline" className='text-zinc-900 w-10'><SearchIcon /></Button>
-            </div>
+            <Form {...searchForm}>
+                <form onSubmit={searchForm.handleSubmit(handleSearch)} className="w-1/4 flex flex-row gap-2 justify-center">
+                    <div className='w-full'>
+                        <FormField
+                        control={searchForm.control}
+                        name="query"
+                        render={({ field }) => (
+                            <FormItem>
+                            <FormControl>
+                                <Input placeholder="Search..." {...field} className='bg-zinc-900 w-full'/>
+                            </FormControl>
+                            <FormMessage />
+                            </FormItem>
+                        )}
+                        />
+                    </div>
+                    
+                    <Button variant="outline" type='submit' className='text-zinc-900 w-10'><SearchIcon /></Button>
+                </form>
+            </Form>
             {
                 isAuth
                 ?
