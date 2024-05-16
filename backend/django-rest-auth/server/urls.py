b0edb3d10e16834c60e17bb3ad3f92853e7c59f3
Oktay Ozel <mail.oktayozel@gmail.com>
2024-05-16 14:21:56 +0300

    deleted backend folder

--- backend/django-rest-auth/server/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from . import views
-
-urlpatterns = [
-    path('signup/', views.signup, name='signup'),
-    path('login/', views.login, name='login'),
-    path('search/', views.search, name='search'),
-]
