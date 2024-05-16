106c2a958735409fef71a357bcc77e90753b60a1
Elifnurdeniz <elfnrdeniz@gmail.com>
2024-05-17 01:53:10 +0300

    done with wiki results

--- frontend/src/pages/Building.tsx
+++ frontend/src/pages/Building.tsx
@@ -13,12 +13,14 @@ import axios from 'axios';
 import BASE_URL from '../lib/baseUrl'
 import ClipLoader from "react-spinners/ClipLoader";
 import ImageNotSupportedIcon from '@mui/icons-material/ImageNotSupported';
+import { useNavigate } from "react-router-dom";
 
 
 export default function Building() {
     let { qid } = useParams();
     const [buildingData, setBuildingData] = useState(null); 
     const [isLoading, setIsLoading] = useState(true)
+    const navigate = useNavigate()
 
 
     useEffect(() => {
@@ -82,24 +84,54 @@ export default function Building() {
                                 <h1 className="font-bold text-4xl">{buildingData.name}</h1>
                                 <Separator />
                                 <p>{buildingData.wikiText}</p>
-                                <p>The site of the Empire State Building, on the west side of Fifth Avenue between West 33rd and 34th Streets, was developed in 1893 as the Waldorf–Astoria Hotel. In 1929, Empire State Inc. acquired the site and devised plans for a skyscraper there. The design for the Empire State Building was changed fifteen times until it was ensured to be the world's tallest building. Construction started on March 17, 1930, and the building opened thirteen and a half months afterward on May 1, 1931. Despite favorable publicity related to the building's construction, because of the Great Depression and World War II, its owners did not make a profit until the early 1950s.</p>
-                                <p>The building's Art Deco architecture, height, and observation decks have made it a popular attraction. Around four million tourists from around the world annually visit the building's 86th- and 102nd-floor observatories; an additional indoor observatory on the 80th floor opened in 2019. The Empire State Building is an international cultural icon: it has been featured in more than 250 television series and films since the film King Kong was released in 1933. The building's size has become the global standard of reference to describe the height and length of other structures. A symbol of New York City, the building has been named as one of the Seven Wonders of the Modern World by the American Society of Civil Engineers. It was ranked first on the American Institute of Architects' List of America's Favorite Architecture in 2007. Additionally, the Empire State Building and its ground-floor interior were designated city landmarks by the New York City Landmarks Preservation Commission in 1980, and were added to the National Register of Historic Places as a National Historic Landmark in 1986.</p>
                             </div>
                         </ScrollArea>
                     </div>
                     <div className="h-screen w-[100vh]">
                         <div className="h-1/2 w-[50vh] pt-6 pl-6 pr-3 pb-3">
-                            <div className="h-full w-full bg-lime-500 rounded-2xl relative">
-                                <img src={buildingData.architect[architectIndex].image} alt="Sagrada Família" className="h-full w-full object-cover rounded-2xl" />
-                                <div className="absolute top-1 left-2 p-3 text-white rounded-md" style={{ borderBottom: '0.2px solid white left-2', backgroundColor: 'rgba(80, 80, 80, 0.5)', padding: '2px 10px', fontSize:18}}>{buildingData.architect[architectIndex].name}</div>
+                            <div className="h-full w-full rounded-2xl relative">
+                                {buildingData.architect[architectIndex].image=="NULL" ? (
+                                    <div className='w-full h-full shadow-sm border rounded-2xl'>
+                                    <ImageNotSupportedIcon  style={{ width: '60%', height: '60%', position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)', color: 'rgba(80, 80, 80, 0.5)'}} />
+
+                                    </div>
+                                ) : (
+                                    <img 
+                                        src={buildingData.architect[architectIndex].image} 
+                                        alt="Sagrada Família" 
+                                        className="h-full w-full object-cover rounded-2xl" 
+                                    />
+                                    
+                                )}
+                                <button onClick={
+                                            () => {
+                                                navigate(`/wiki/architect/${buildingData.architect[architectIndex].id}`);
+                                                navigate(0);
+                                            }} className="absolute top-1 left-2 p-3 text-white rounded-md" style={{ borderBottom: '0.2px solid white left-2', backgroundColor: 'rgba(80, 80, 80, 0.5)', padding: '2px 10px', fontSize:18}}>{buildingData.architect[architectIndex].name}</button>
                                 <Button className="absolute bottom-1 left-2 p-3 text-white" variant="prev" onClick={handleArchitectPrevClick} style={{ borderBottom: '0.2px solid white left-2', backgroundColor: 'rgba(80, 80, 80, 0.5)', padding: '2px 10px', fontSize:18 }}><ArrowBackIosIcon/></Button>
                                 <Button className="absolute bottom-1 right-2 p-3 text-white" variant="next" onClick={handleArchitectNextClick} style={{ borderBottom: '0.2px solid white left-2', backgroundColor: 'rgba(80, 80, 80, 0.5)', padding: '2px 10px', fontSize:18 }}><ArrowForwardIosIcon/></Button>
                             </div>
                         </div>
                         <div className="h-1/2 w-[50vh] pb-6 pl-6 pr-3 pt-3">
-                            <div className="h-full w-full bg-red-500 rounded-2xl relative">
-                                <img src={buildingData.architecturalStyle[artIndex].image} alt="Sagrada Família" className="h-full w-full object-cover rounded-2xl" />
-                                <div className="absolute top-1 left-2 p-3 text-white rounded-md" style={{ borderBottom: '0.2px solid white left-2', backgroundColor: 'rgba(80, 80, 80, 0.5)', padding: '2px 10px', fontSize:18 }}>{buildingData.architecturalStyle[artIndex].name}</div>
+                            <div className="h-full w-full  rounded-2xl relative">
+                                {buildingData.architecturalStyle[artIndex].image=="NULL" ? (
+                                    <div className='w-full h-full shadow-sm border rounded-2xl'>
+                                    <ImageNotSupportedIcon  style={{ width: '60%', height: '60%', position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)', color: 'rgba(80, 80, 80, 0.5)'}} />
+
+                                    </div>
+                                ) : (
+                                    <img 
+                                        src={buildingData.architecturalStyle[artIndex].image} 
+                                        alt="Sagrada Família" 
+                                        className="h-full w-full object-cover rounded-2xl" 
+                                    />
+                                    
+                                )}
+                                <button onClick={
+                                            () => {
+                                                navigate(`/wiki/style/${buildingData.architecturalStyle[artIndex].id}`);
+                                                navigate(0);
+                                            }} className="absolute top-1 left-2 p-3 text-white rounded-md" style={{ borderBottom: '0.2px solid white left-2', backgroundColor: 'rgba(80, 80, 80, 0.5)', padding: '2px 10px', fontSize:18 }}>{buildingData.architecturalStyle[artIndex].name}</button>
                                 <Button className="absolute bottom-1 left-2 p-3 text-white" variant="prev" onClick={handleArtPrevClick} style={{ borderBottom: '0.2px solid white left-2', backgroundColor: 'rgba(80, 80, 80, 0.5), fontSize:18', padding: '2px 10px', fontSize:18 }}><ArrowBackIosIcon/></Button>
                                 <Button className="absolute bottom-1 right-2 p-3 text-white" variant="next" onClick={handleArtNextClick} style={{ borderBottom: '0.2px solid white left-2', backgroundColor: 'rgba(80, 80, 80, 0.5)', padding: '2px 10px', fontSize:18}}><ArrowForwardIosIcon/></Button>
                             </div>
@@ -107,8 +139,20 @@ export default function Building() {
                     </div>
                     <div className="h-screen w-[100vh]">
                         <div className="h-1/2 w-[50vh] pt-6 pr-6 pl-3 pb-3">
-                            <div className="h-full w-full bg-zinc-500 rounded-2xl relative">
-                                <img src={buildingData.image[galleryIndex]} alt="Sagrada Família" className="h-full w-full object-cover rounded-2xl" />
+                            <div className="h-full w-full rounded-2xl relative">
+                                {buildingData.image[galleryIndex]=="NULL" ? (
+                                    <div className='w-full h-full shadow-sm border rounded-2xl'>
+                                    <ImageNotSupportedIcon  style={{ width: '60%', height: '60%', position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)', color: 'rgba(80, 80, 80, 0.5)'}} />
+
+                                    </div>
+                                ) : (
+                                    <img 
+                                        src={buildingData.image[galleryIndex]} 
+                                        alt="Sagrada Família" 
+                                        className="h-full w-full object-cover rounded-2xl" 
+                                    />
+                                    
+                                )}
                                 <div className="absolute top-1 left-2 p-3 text-white rounded-md" style={{ borderBottom: '0.2px solid white left-2', backgroundColor: 'rgba(80, 80, 80, 0.5)', padding: '2px 10px', fontSize:18}}>Gallery</div>
                                 <Button className="absolute bottom-1 left-2 p-3 text-white" variant="prev" onClick={handleGalleryPrevClick} style={{ borderBottom: '0.2px solid white left-2', backgroundColor: 'rgba(80, 80, 80, 0.5)', padding: '2px 10px', fontSize:18 }}><ArrowBackIosIcon/></Button>
                                 <Button className="absolute bottom-1 right-2 p-3 text-white" variant="next" onClick={handleGalleryNextClick} style={{ borderBottom: '0.2px solid white left-2', backgroundColor: 'rgba(80, 80, 80, 0.5)', padding: '2px 10px', fontSize:18 }}><ArrowForwardIosIcon/></Button>
