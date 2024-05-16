71e67d7f61206c855871c9c7e5ca4ffce2c308e0
yunusemreozdemir <emreozdemir@mail.com>
2024-05-17 01:27:49 +0300

    feat: get feed

--- frontend/src/pages/Feed.tsx
+++ frontend/src/pages/Feed.tsx
@@ -1,12 +1,25 @@
 import { Button } from '@/components/ui/button'
 import { Navbar } from '../components'
 import { Post } from '../components'
-
+import { useEffect, useState } from 'react'
 import { useAuth } from '../hooks'
+import axios from 'axios'
 
 export default function Feed() {
     const { checkAuth } = useAuth()
     const isAuth = checkAuth()
+    const [postIDs, setPostIDs] = useState([])
+
+    useEffect(() => {
+        axios.get('http://localhost:8000/guest_feed/')
+            .then(response => {
+                console.log(response.data.post_ids)
+                setPostIDs(response.data.post_ids)
+            })
+            .catch(error => {
+                console.log(error)
+            })
+    }, [])
 
     return (
         <div className="flex flex-col">
@@ -14,12 +27,13 @@ export default function Feed() {
             <div className="flex flex-row justify-center w-full">
                 <div className="w-5/12">
                     {
-                        isAuth
-                        ?
-                            <Post />
-                        :
-                            null
-                            
+                        postIDs.map((postID) => {
+                            return (
+                                <div key={postID}>
+                                    <Post postID={postID} />
+                                </div>
+                            )
+                        })
                     }
                 </div>
             </div>
