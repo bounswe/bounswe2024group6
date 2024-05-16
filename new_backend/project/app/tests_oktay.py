86b2f4d12ef65e4d29ca752cc2de88b599ece2ce
Oktay Ozel <mail.oktayozel@gmail.com>
2024-05-16 15:35:25 +0300

    updated login and signup test

--- new_backend/project/app/tests_oktay.py
+++ new_backend/project/app/tests_oktay.py
@@ -12,14 +12,16 @@ class UserTestCase(TestCase):
         # Create a user using the CustomUser model
         self.user = CustomUser.objects.create_user(**self.user_data)
 
+ 
+
     def test_login(self):
-        url = '/login/'  # URL for the login view
+        url = reverse('user_login')  # URL for the login view
         response = self.client.post(url, self.user_data)
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, 'token')
 
     def test_invalid_login(self):
-        url = '/login/'  # URL for the login view
+        url = reverse('user_login')  # URL for the login view
         invalid_data = {
             'username': 'invalid_user',
             'password': 'invalid_password'
