2977319a44807ffc97e40127cc6e48553ed76efd
Elifnurdeniz <elfnrdeniz@gmail.com>
2024-05-16 18:34:21 +0300

    changes in wiki requests

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -4,3 +4,5 @@ export { default as Profile } from './Profile'
 export { default as Architect } from './Architect'
 export { default as Building } from './Building'
 export { default as Style } from './Style'
+export { default as Browse } from './Browse'
+
