c2812df9daaddea680abc7cd2edebc48b8ebd596
arastasci <arastasci@gmail.com>
2024-10-19 19:29:56 +0300

    refactor mock data initialization

---
+++ backend/app/mock_data_manager.py
@@ -0,0 +1,204 @@
+from .quiz_manager import CreateQuiz
+
+def create_quiz_mockdata():
+    from .quiz_manager import CreateQuiz
+    
+    CreateQuiz(
+        title="Food",
+        description="Learn about foods",
+        author="Oguz",
+        level="A2",
+        text="This is a quiz",
+        time_limit=10,
+        i=1
+    )
+
+    CreateQuiz(
+        title="Animals",
+        description="Our furry friends!",
+        author="Aras",
+        level="A2",
+        text="This is a quiz",
+        time_limit=10,
+        i=2
+    )
+
+    CreateQuiz(
+        title="Furniture",
+        description="Essential furniture",
+        author="Kaan",
+        level="A2",
+        text="This is a quiz",
+        time_limit=10,
+        i=3
+    )
+
+    CreateQuiz(
+        title="Plants",
+        description="Test your plant knowledge",
+        author="Halil",
+        level="A2",
+        text="This is a quiz",
+        time_limit=10,
+        i=4
+    )
+
+    CreateQuiz(
+        title="Transport",
+        description="Types of transport",
+        author="Alex",
+        level="B1",
+        text="This is a quiz",
+        time_limit=10,
+        i=5
+    )
+
+    CreateQuiz(
+        title="Food",
+        description="Learn about foods",
+        author="Oguz",
+        level="A2",
+        text="This is a quiz",
+        time_limit=10,
+        i=6
+    )
+
+    CreateQuiz(
+        title="Animals",
+        description="Our furry friends!",
+        author="Aras",
+        level="A2",
+        text="This is a quiz",
+        time_limit=10,
+        i=7
+    )
+
+    CreateQuiz(
+        title="Furniture",
+        description="Essential furniture",
+        author="Kaan",
+        level="A2",
+        text="This is a quiz",
+        time_limit=10,
+        i=8
+    )
+
+    CreateQuiz(
+        title="Plants",
+        description="Test your plant knowledge",
+        author="Halil",
+        level="A2",
+        text="This is a quiz",
+        time_limit=10,
+        i=9
+    )
+
+    CreateQuiz(
+        title="Transport",
+        description="Types of transport",
+        author="Alex",
+        level="B1",
+        text="This is a quiz",
+        time_limit=10,
+        i=10
+    )
+
+    CreateQuiz(
+        title="Food",
+        description="Learn about foods",
+        author="Oguz",
+        level="A2",
+        text="This is a quiz",
+        time_limit=10,
+        i=11
+    )
+
+    CreateQuiz(
+        title="Animals",
+        description="Our furry friends!",
+        author="Aras",
+        level="A2",
+        text="This is a quiz",
+        time_limit=10,
+        i=12
+    )
+
+    CreateQuiz(
+        title="Furniture",
+        description="Essential furniture",
+        author="Kaan",
+        level="A2",
+        text="This is a quiz",
+        time_limit=10,
+        i=13
+    )
+
+    CreateQuiz(
+        title="Plants",
+        description="Test your plant knowledge",
+        author="Halil",
+        level="A2",
+        text="This is a quiz",
+        time_limit=10,
+        i=14
+    )
+
+    CreateQuiz(
+        title="Transport",
+        description="Types of transport",
+        author="Alex",
+        level="B1",
+        text="This is a quiz",
+        time_limit=10,
+        i=15
+    )
+
+    CreateQuiz(
+        title="Food",
+        description="Learn about foods",
+        author="Oguz",
+        level="A2",
+        text="This is a quiz",
+        time_limit=10,
+        i=16
+    )
+
+    CreateQuiz(
+        title="Animals",
+        description="Our furry friends!",
+        author="Aras",
+        level="A2",
+        text="This is a quiz",
+        time_limit=10,
+        i=17
+    )
+
+    CreateQuiz(
+        title="Furniture",
+        description="Essential furniture",
+        author="Kaan",
+        level="A2",
+        text="This is a quiz",
+        time_limit=10,
+        i=18
+    )
+
+    CreateQuiz(
+        title="Plants",
+        description="Test your plant knowledge",
+        author="Halil",
+        level="A2",
+        text="This is a quiz",
+        time_limit=10,
+        i=19
+    )
+
+    CreateQuiz(
+        title="Transport",
+        description="Types of transport",
+        author="Alex",
+        level="B1",
+        text="This is a quiz",
+        time_limit=10,
+        i=20
+    )
