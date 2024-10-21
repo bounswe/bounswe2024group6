ac9de70ff8dd0ef76189ca4339bec9987c28a4e2
Elifnurdeniz <elfnrdeniz@gmail.com>
2024-10-21 11:44:53 +0300

    added posts part

--- frontend/src/pages/profile.tsx
+++ frontend/src/pages/profile.tsx
@@ -1,34 +1,145 @@
 import { Navbar } from "../components/common";
 import { Avatar, Button, Divider, Card } from "@nextui-org/react";
+import { Suspense, useState } from "react";
+import { PostCard, PostCardSkeleton } from "../components/post";
 
 export default function Profile() {
+  const [activeSection, setActiveSection] = useState("quizzes");
+
+  const mockData = {
+    "id": 1,
+    "username": "oktay_ozel",
+    "level": "B2",
+    "followers": 100,
+    "following": 50,
+    "image": "https://nextui.org/avatars/avatar-1.png",
+    "posts": [{
+      "id": 2,
+      "author": {
+        "username": "oktay_ozel",
+        "profile_image": "https://private-user-images.githubusercontent.com/57640531/310137517-cbe7aa9f-3457-4f64-b37b-c3e46d4e448b.png?jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJnaXRodWIuY29tIiwiYXVkIjoicmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbSIsImtleSI6ImtleTUiLCJleHAiOjE3MjkzMjk2NzUsIm5iZiI6MTcyOTMyOTM3NSwicGF0aCI6Ii81NzY0MDUzMS8zMTAxMzc1MTctY2JlN2FhOWYtMzQ1Ny00ZjY0LWIzN2ItYzNlNDZkNGU0NDhiLnBuZz9YLUFtei1BbGdvcml0aG09QVdTNC1ITUFDLVNIQTI1NiZYLUFtei1DcmVkZW50aWFsPUFLSUFWQ09EWUxTQTUzUFFLNFpBJTJGMjAyNDEwMTklMkZ1cy1lYXN0LTElMkZzMyUyRmF3czRfcmVxdWVzdCZYLUFtei1EYXRlPTIwMjQxMDE5VDA5MTYxNVomWC1BbXotRXhwaXJlcz0zMDAmWC1BbXotU2lnbmF0dXJlPWY1YTNiM2MxZDY0ZTdjYmNlZTU2NmU5NTUyZDZjMjI4NGZmZjNmNGI2OWI2ZjljODg1MzFmOThhOTEwYjFmYzgmWC1BbXotU2lnbmVkSGVhZGVycz1ob3N0In0.GctR1cVt7p3T5MUGBCWRNT_9kdB5SRXamcQVh5pLMOc"
+      },
+      "post": {
+        "title": "Petrichor: The Smell of Rain",
+        "content": "Petrichor is the pleasant, earthy smell that comes after rain. Example: 'She loved the petrichor that filled the air after the storm.'",
+        "category": "Vocabulary",
+        "timestamp": "5 hours ago"
+      },
+      "engagement": {
+        "likes": 90,
+        "comments": 3,
+        "bookmark": 3
+      }
+    },
+    {
+      "id": 3,
+      "author": {
+        "username": "oktay_ozel",
+        "profile_image": "https://private-user-images.githubusercontent.com/57640531/310137517-cbe7aa9f-3457-4f64-b37b-c3e46d4e448b.png?jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJnaXRodWIuY29tIiwiYXVkIjoicmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbSIsImtleSI6ImtleTUiLCJleHAiOjE3MjkzMjk2NzUsIm5iZiI6MTcyOTMyOTM3NSwicGF0aCI6Ii81NzY0MDUzMS8zMTAxMzc1MTctY2JlN2FhOWYtMzQ1Ny00ZjY0LWIzN2ItYzNlNDZkNGU0NDhiLnBuZz9YLUFtei1BbGdvcml0aG09QVdTNC1ITUFDLVNIQTI1NiZYLUFtei1DcmVkZW50aWFsPUFLSUFWQ09EWUxTQTUzUFFLNFpBJTJGMjAyNDEwMTklMkZ1cy1lYXN0LTElMkZzMyUyRmF3czRfcmVxdWVzdCZYLUFtei1EYXRlPTIwMjQxMDE5VDA5MTYxNVomWC1BbXotRXhwaXJlcz0zMDAmWC1BbXotU2lnbmF0dXJlPWY1YTNiM2MxZDY0ZTdjYmNlZTU2NmU5NTUyZDZjMjI4NGZmZjNmNGI2OWI2ZjljODg1MzFmOThhOTEwYjFmYzgmWC1BbXotU2lnbmVkSGVhZGVycz1ob3N0In0.GctR1cVt7p3T5MUGBCWRNT_9kdB5SRXamcQVh5pLMOc"
+      },
+      "post": {
+        "title": "Serendipity: A Fortunate Discovery",
+        "content": "Serendipity is the occurrence of events by chance in a happy or beneficial way. Example: 'They met by serendipity and became lifelong friends.'",
+        "category": "Vocabulary",
+        "timestamp": "2 hours ago"
+      },
+      "engagement": {
+        "likes": 75,
+        "comments": 5,
+        "bookmark": 4
+      }
+    }],
+    "quizzes": [
+      {
+        "id": 1,
+        "title": "Basic Vocabulary Quiz",
+        "description": "Test your knowledge on basic vocabulary words!",
+        "author": "oktay_ozel",
+        "upvote": 5,
+        "level": "Beginner"
+      },
+      {
+        "id": 2,
+        "title": "Advanced Vocabulary Quiz",
+        "description": "Challenge yourself with advanced vocabulary words.",
+        "author": "oktay_ozel",
+        "upvote": 9,
+        "level": "Advanced"
+      }
+    ]
+  }
+
+  const quizCards = (
+    <div className="p-5">
+      <p className="p-3 mb-3">Not implemented yet</p>
+      <div className="border p-3 mb-3 rounded-md">Quiz 1</div>
+      <div className="border p-3 mb-3 rounded-md">Quiz 2</div>
+      <div className="border p-3 mb-3 rounded-md">Quiz 3</div>
+    </div>
+  );
+
+  const postCards = (
+    <div className="flex flex-col my-5 items-center">
+      {mockData.posts.map((post) => (
+        <Suspense key={post.id} fallback={<PostCardSkeleton />}>
+          <PostCard
+            key={post.id}
+            id={post.id}
+            username={post.author.username}
+            content={post.post.content}
+            timePassed={post.post.timestamp}
+            likeCount={post.engagement.likes}
+            tags={[post.post.category]}  // if exists
+          />
+        </Suspense>
+      ))}
+    </div>
+  );
+
   return (
     <div className="h-screen w-screen flex flex-col">
       <Navbar />
       <div className="flex justify-between items-center w-full px-32 py-5">
         <div className="flex items-center px-5 bg-white rounded-lg">
-          {/* Avatar on the left */}
           <Avatar
-            src="https://cdn.evrimagaci.org/q0-4ffcpiHlsmEHyfYCcYQBWPNg=/storage.evrimagaci.org%2Fold%2Fmi_media%2Fafcae823e61eefb077e1f223594b1e7f.jpeg"
+            src="{mockData.image}"
             className="mr-3 w-24 h-24"
           />
           <div className="ml-4">
-            <h3 className="text-xl font-semibold">Elifnd</h3>
-            <p className="text-gray-500">@B1</p>
+            <h3 className="text-xl font-semibold">{mockData.username}</h3>
+            <p className="text-gray-500">@{mockData.level}</p>
           </div>
         </div>
-        <Button className="border-2 border-blue-900 text-blue-900 font-bold bg-white px-8 py-6 rounded-md">150 Following</Button>
-        <Button className="border-2 border-blue-900 text-blue-900 font-bold bg-white px-8 py-6 rounded-md">200 Followers</Button>
+        <Button className="border-2 border-blue-900 text-blue-900 font-bold bg-white px-8 py-6 rounded-md">{mockData.following} Following</Button>
+        <Button className="border-2 border-blue-900 text-blue-900 font-bold bg-white px-8 py-6 rounded-md">{mockData.followers} Followers</Button>
         <Button className="border-2 border-blue-900 text-blue-900 font-bold bg-white px-8 py-6 rounded-md">Solved Quizzes</Button>
       </div>
       <Divider className="my-4 border-t-2 border-gray-400 border-dashed" />
-      <Card className="w-48 border-2 border-blue-900 items-center mx-auto my-3">
+      <Card className="border-1 border-blue-900 items-center pt-5 pb-10 mx-auto my-3">
         <div className="flex flex-row w-full justify-between items-center">
-          <Button className="text-blue-900 font-bold bg-white px-8 py-6 rounded-md">Quizzes</Button>
-          <p className="text-blue-900 font-bold bg-white rounded-md">/</p>
-          <Button className="text-blue-900 font-bold bg-white px-8 py-6 rounded-md">Posts</Button>
+          <Button
+            className={`text-blue-900 font-bold text-lg bg-white px-6 h-full rounded-md ${activeSection === "quizzes" ? "font-extrabold border-b-2 border-zinc-300" : ""
+              }`}
+            onClick={() => setActiveSection("quizzes")}
+          >
+            Quizzes
+          </Button>
+          <p className="text-blue-900 font-bold bg-white rounded-md mx-2">/</p>
+          <Button
+            className={`text-blue-900 font-bold text-lg bg-white px-6 h-full rounded-md ${activeSection === "posts" ? "font-extrabold border-b-2 border-zinc-300" : ""
+              }`}
+            onClick={() => setActiveSection("posts")}
+          >
+            Posts
+          </Button>
         </div>
       </Card>
+      <div className="flex flex-col p-5 items-center">
+        {activeSection === "quizzes" ? quizCards : postCards}
+      </div>
     </div>
   );
+
+
+
 }
