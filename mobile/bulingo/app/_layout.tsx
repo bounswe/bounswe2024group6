65ef7bb514c2d703e64bb34e4a27b3468d9c859c
YavizGuldalf <yagiz.guldal@outlook.com>
2024-10-27 22:41:33 +0300

    fix(mobile): indentation fix

--- mobile/bulingo/app/_layout.tsx
+++ mobile/bulingo/app/_layout.tsx
@@ -5,27 +5,27 @@ export default function Layout() {
 
     console.log("Layout run")
   return (
-      <Stack
-        screenOptions={{
-          headerStyle: {
-            backgroundColor: '#f4511e',
-          },
-          headerTintColor: '#fff',
-          headerTitleStyle: {
-            fontWeight: 'bold',
-          },
-        }}>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="quizFeed" options={{headerShown: false}} />
-        <Stack.Screen name="register" options={{headerShown: false}} />
-        <Stack.Screen name="quizDetails" options={{headerShown: false}} />
-        <Stack.Screen name="login" options={{headerShown: false}}/>
-        <Stack.Screen name="quizResults" options={{headerShown: false}}/>
-        {/* <Stack.Screen name="index" options={{headerShown: false}}/> */}
-        <Stack.Screen name="quizQuestion" options={{headerShown: false}}/>
-        <Stack.Screen name="quizCreationQuestionList" options={{headerShown: false}}/>
-        <Stack.Screen name="quizCreationSettings" options={{headerShown: false}}/>
-        <Stack.Screen name="quizCreationInfo" options={{headerShown: false}}/>
-      </Stack>
+    <Stack
+      screenOptions={{
+        headerStyle: {
+          backgroundColor: '#f4511e',
+        },
+        headerTintColor: '#fff',
+        headerTitleStyle: {
+          fontWeight: 'bold',
+        },
+      }}>
+      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+      <Stack.Screen name="quizFeed" options={{headerShown: false}} />
+      <Stack.Screen name="register" options={{headerShown: false}} />
+      <Stack.Screen name="quizDetails" options={{headerShown: false}} />
+      <Stack.Screen name="login" options={{headerShown: false}}/>
+      <Stack.Screen name="quizResults" options={{headerShown: false}}/>
+      {/* <Stack.Screen name="index" options={{headerShown: false}}/> */}
+      <Stack.Screen name="quizQuestion" options={{headerShown: false}}/>
+      <Stack.Screen name="quizCreationQuestionList" options={{headerShown: false}}/>
+      <Stack.Screen name="quizCreationSettings" options={{headerShown: false}}/>
+      <Stack.Screen name="quizCreationInfo" options={{headerShown: false}}/>
+    </Stack>
   );
 }
