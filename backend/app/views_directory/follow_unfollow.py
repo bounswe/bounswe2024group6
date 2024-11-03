fea0d10b78dff62b0ec5b40c234edaea3a04219f
Oktay Özel <oozel@Oktays-MacBook-Pro.local>
2024-11-03 17:29:36 +0300

    follow-unfollow functinonalities and admin panel to check them

---
+++ backend/app/views_directory/follow_unfollow.py
@@ -0,0 +1,29 @@
+from django.shortcuts import get_object_or_404
+from django.http import JsonResponse
+from app.models import Profile
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
+
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def follow_user(request):
+    current_user_profile = request.user.profile
+    user_id = request.data.get("user_id")  # Get the user_id from the request body
+    user_to_follow = get_object_or_404(Profile, user__id=user_id)
+
+    if user_to_follow != current_user_profile:
+        current_user_profile.following.add(user_to_follow)
+        return JsonResponse({"message": "Followed successfully"}, status=200)
+    return JsonResponse({"error": "Cannot follow yourself"}, status=400)
+
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def unfollow_user(request):
+    current_user_profile = request.user.profile
+    user_id = request.data.get("user_id")  # Get the user_id from the request body
+    user_to_unfollow = get_object_or_404(Profile, user__id=user_id)
+
+    if user_to_unfollow != current_user_profile:
+        current_user_profile.following.remove(user_to_unfollow)
+        return JsonResponse({"message": "Unfollowed successfully"}, status=200)
+    return JsonResponse({"error": "Cannot unfollow yourself"}, status=400)
