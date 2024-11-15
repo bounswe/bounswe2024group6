41ea298d6e93a37c4b4974a77bc469be8da97ecb
Ali Tarık Şahin <alitariksah@gmail.com>
2024-11-15 22:25:42 +0300

    feat(frontend): created register logic

---
+++ frontend/src/components/auth/register.tsx
@@ -0,0 +1,149 @@
+"use client";
+import { Button, Input } from "@nextui-org/react";
+import { useMemo, useState } from "react";
+import { useForm } from "react-hook-form";
+import { AuthActions } from "./utils";
+
+type FormData = {
+  email: string;
+  username: string;
+  password: string;
+};
+
+export default function Register({
+  setIsRegister,
+}: {
+  setIsRegister: (value: boolean) => void;
+}) {
+  const [username, setUserName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    setError,
+  } = useForm<FormData>();
+
+  const { register: registerUser } = AuthActions(); // Note: Renamed to avoid naming conflict with useForm's register
+
+  const onSubmit = () => {
+    registerUser(email, username, password)
+      .json(() => {
+        setIsRegister(false);
+      })
+      .catch((err) => {
+        setError("root", {
+          type: "manual",
+          message: err.json.detail,
+        });
+      });
+  };
+
+  const validatePassword = (password: string) => password.match(/^.{8,}$/i);
+
+  const isInvalidPassword = useMemo(() => {
+    if (password === "") return false;
+
+    return validatePassword(password) ? false : true;
+  }, [password]);
+
+  const validateConfirmPassword = (confirmPassword: string) =>
+    confirmPassword.match(password);
+
+  const isInvalidConfirmPassword = useMemo(() => {
+    if (confirmPassword === "") return false;
+
+    return validateConfirmPassword(confirmPassword) ? false : true;
+  }, [confirmPassword]);
+
+  return (
+    <div className="lg:w-1/3 flex pr-24 pl-12 lg:justify-end items-center h-screen">
+      <div className="flex flex-col items-center w-full max-w-lg p-6">
+        <h2 className="text-2xl font-semibold text-center mb-6">Join Us</h2>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <div className="mb-4">
+            <Input
+              type="text"
+              isRequired
+              label="Username"
+              onChange={(e) => setUserName(e.target.value)}
+            />
+          </div>
+          <div className="mb-4">
+            <Input
+              type="email"
+              isRequired
+              label="Email"
+              {...register("email", { required: "Email is required" })}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            {errors.email && (
+              <span className="text-xs text-red-600">
+                {errors.email.message}
+              </span>
+            )}
+          </div>
+          <div className="mb-4">
+            <Input
+              label="Password"
+              variant="bordered"
+              isRequired
+              {...register("password", { required: "Password is required" })}
+              isInvalid={isInvalidPassword}
+              color={isInvalidPassword ? "danger" : "default"}
+              errorMessage="Password must be at least 8 characters"
+              onChange={(e) => setPassword(e.target.value)}
+              type="password"
+              className="w-56"
+            />
+            {errors.password && (
+              <span className="text-xs text-red-600">
+                {errors.password.message}
+              </span>
+            )}
+          </div>
+          <div className="mb-4">
+            <Input
+              label="Confirm Password"
+              variant="bordered"
+              isRequired
+              isInvalid={isInvalidConfirmPassword}
+              color={isInvalidConfirmPassword ? "danger" : "default"}
+              errorMessage="Passwords do not match"
+              onChange={(e) => setConfirmPassword(e.target.value)}
+              type="password"
+              className="w-56"
+            />
+          </div>
+          <Button
+            color="primary"
+            className="w-full"
+            type="submit"
+            isDisabled={
+              username === ""
+                ? true
+                : email === "" ||
+                  password === "" ||
+                  isInvalidPassword ||
+                  isInvalidConfirmPassword
+            }
+          >
+            Sign Up
+          </Button>
+        </form>
+        <div className="text-center mt-6">
+          <span className="text-sm">Already have an account? </span>
+          <button
+            onClick={() => setIsRegister(false)}
+            className="text-blue-600 hover:underline"
+          >
+            Login
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
