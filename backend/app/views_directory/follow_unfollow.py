980ef753f41c59cb54eaa1e76c4536998c210c85
Oktay Özel <mail.oktayozel@gmail.com>
2024-11-19 16:10:48 +0300

    Update follow_unfollow.py

--- backend/app/views_directory/follow_unfollow.py
+++ backend/app/views_directory/follow_unfollow.py
@@ -1,6 +1,6 @@
 from django.shortcuts import get_object_or_404
 from django.http import JsonResponse
-from app.models import Profile
+from app.models import Profile, ActivityStream
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated
 
@@ -13,6 +13,15 @@ def follow_user(request):
 
     if user_to_follow != current_user_profile:
         current_user_profile.following.add(user_to_follow)
+
+        ActivityStream.objects.create(
+            actor=request.user,
+            verb="followed",
+            object_type="Profile",
+            object_id=user_to_follow.user.id,
+            target=None  # Optional: could be used for specific contexts
+        )
+
         return JsonResponse({"message": "Followed successfully"}, status=200)
     return JsonResponse({"error": "Cannot follow yourself"}, status=400)
 
