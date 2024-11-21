8ec808ca708c3e38223e54e5e73d3be9a8545fb0
Elifnurdeniz <elfnrdeniz@gmail.com>
2024-11-21 20:37:38 +0300

    feat(frontend): redesigned quiz component and added photo to it

--- frontend/src/components/quiz/quiz-card.tsx
+++ frontend/src/components/quiz/quiz-card.tsx
@@ -25,6 +25,7 @@ type Props = {
   username: string;
   title: string;
   content: string;
+  picture?: string;
   timePassed: string;
   likeCount: number;
   tags: string[];
@@ -35,6 +36,7 @@ export default function PostCard({
   username,
   title,
   content,
+  picture = "https://nextui.org/avatars/avatar-1.png",
   timePassed,
   likeCount,
   tags,
@@ -65,7 +67,7 @@ export default function PostCard({
 
   return (
     <Card className="w-[740px] px-2 pt-2 bg-white">
-      <CardHeader className="flex flex-col items-start gap-2 mb-2">
+      <CardHeader className="flex flex-col items-start gap-2">
         <div className="flex w-full justify-between">
           <div className="flex gap-3">
             <Avatar
@@ -82,84 +84,89 @@ export default function PostCard({
           </div>
           <p className="text-default-400 text-small">{timePassed}</p>
         </div>
-        <Divider className="my-1.5 bg-zinc-200" />
-        <div className="flex flex-row justify-between w-full">
-          <h2 className="text-2xl font-semibold leading-none text-black">
-            <Link to={`/quiz/${id}/details`} className="text-black hover:underline">
-              {title}
-            </Link>
-          </h2>
-          <p className="text-default-600 text-sm">15 Attemps</p>
-        </div>
+        <Divider className="mt-1.5 bg-zinc-200" />
       </CardHeader>
-      <CardBody className="px-3 py-0 text-small text-default-600 text-justify leading-relaxed overflow-hidden">
-        <p>
-          {displayedText}
-          {content.length > maxLength && (
-            <span
-              onClick={toggleExpand}
-              style={{ color: "#186df5", cursor: "pointer" }}
-            >
-              {isExpanded ? " Read less" : " Read more"}
-            </span>
-          )}
-        </p>
-      </CardBody>
-      <CardFooter className="flex justify-between gap-2">
-        <div className="flex gap-0 items-center">
-          <div className="flex justify-between items-center">
-            <p
-              className={cn("font-semibold text-default-500", {
-                "font-semibold text-red-500": isLiked,
-              })}
-            >
-              {likes}
-            </p>
-            <Button
-              isIconOnly
-              color="danger"
-              aria-label="Like"
-              onClick={toggleLike}
-              variant="light"
-              className="flex items-center gap-3"
-            >
-              {isLiked ? (
-                <IconThumbUpFilled size={20} stroke={1.5} />
-              ) : (
-                <IconThumbUp size={20} stroke={1.5} />
+      <div className="flex flex-row justify-between items-center mb-5">
+        <img src={picture} style={{ width: "200px", height: "200px", objectFit: "cover", objectPosition: "center" }} ></img>
+        <div className="w-[500px] flex flex-col justify-between h-full pt-4">
+          <CardBody className="px-3 py-0 text-small text-default-600 text-justify leading-relaxed overflow-hidden">
+            <div className="flex flex-row justify-between w-full">
+              <h2 className="text-2xl font-semibold leading-none text-black">
+                <Link to={`/quiz/${id}/details`} className="text-black hover:underline">
+                  {title}
+                </Link>
+              </h2>
+              <p className="text-default-600 text-sm">15 Attemps</p>
+            </div>
+            <p>
+              {displayedText}
+              {content.length > maxLength && (
+                <span
+                  onClick={toggleExpand}
+                  style={{ color: "#186df5", cursor: "pointer" }}
+                >
+                  {isExpanded ? " Read less" : " Read more"}
+                </span>
               )}
-            </Button>
-          </div>
-          <Button
-            isIconOnly
-            color="secondary"
-            aria-label="Bookmark"
-            onClick={toggleBookmark}
-            variant="light"
-          >
-            {isBookmarked ? (
-              <IconBookmarkFilled size={20} stroke={1.5} />
-            ) : (
-              <IconBookmark size={20} stroke={1.5} />
-            )}
-          </Button>
-        </div>
-        <div className="flex gap-2">
-          {tags &&
-            tags.map((tag) => (
+            </p>
+          </CardBody>
+          <CardFooter className="flex justify-between gap-2">
+            <div className="flex gap-0 items-center">
+              <div className="flex justify-between items-center">
+                <p
+                  className={cn("font-semibold text-default-500", {
+                    "font-semibold text-red-500": isLiked,
+                  })}
+                >
+                  {likes}
+                </p>
+                <Button
+                  isIconOnly
+                  color="danger"
+                  aria-label="Like"
+                  onClick={toggleLike}
+                  variant="light"
+                  className="flex items-center gap-3"
+                >
+                  {isLiked ? (
+                    <IconThumbUpFilled size={20} stroke={1.5} />
+                  ) : (
+                    <IconThumbUp size={20} stroke={1.5} />
+                  )}
+                </Button>
+              </div>
               <Button
-                key={tag}
-                color="primary"
-                variant="flat"
-                className="text-sm h-8"
-                size="sm"
-                radius="full"
+                isIconOnly
+                color="secondary"
+                aria-label="Bookmark"
+                onClick={toggleBookmark}
+                variant="light"
               >
-                {tag}
+                {isBookmarked ? (
+                  <IconBookmarkFilled size={20} stroke={1.5} />
+                ) : (
+                  <IconBookmark size={20} stroke={1.5} />
+                )}
               </Button>
-            ))}
+            </div>
+            <div className="flex gap-2">
+              {tags &&
+                tags.map((tag) => (
+                  <Button
+                    key={tag}
+                    color="primary"
+                    variant="flat"
+                    className="text-sm h-8"
+                    size="sm"
+                    radius="full"
+                  >
+                    {tag}
+                  </Button>
+                ))}
+            </div>
+          </CardFooter>
         </div>
-      </CardFooter>
+      </div>
     </Card>
   );
 }
