bef86a2c8fbf469d622617b823c4c5cdc1cf3cf4
yunusemreozdemir <emreozdemir@mail.com>
2024-11-21 20:08:44 +0300

    feat: embed backend url

--- frontend/src/lib/baseURL.ts
+++ frontend/src/lib/baseURL.ts
@@ -1,4 +1,4 @@
-const BACKEND_URL: string = "http://localhost";
+const BACKEND_URL: string = "http://161.35.208.249";
 const BACKEND_PORT: string = "8000";
 
 export const BASE_URL: string = `${BACKEND_URL}:${BACKEND_PORT}`;
