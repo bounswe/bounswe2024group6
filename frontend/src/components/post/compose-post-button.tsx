ec7243668550f55e2f9fbc50d5b2b72198381388
yunusemreozdemir <emreozdemir@mail.com>
2024-11-21 20:05:54 +0300

    feat: restyle compose post button

--- frontend/src/components/post/compose-post-button.tsx
+++ frontend/src/components/post/compose-post-button.tsx
@@ -1,20 +1,17 @@
-import { Button } from "@nextui-org/react";
 import { IconPencil } from "@tabler/icons-react";
 import { useNavigate } from "react-router-dom";
 
 const ComposePostButton = () => {
   const navigate = useNavigate();
   return (
-    <Button
-      isIconOnly
+    <div
       onClick={() => navigate("/compose-post")}
       color="primary"
-      variant="flat"
       aria-label="Post"
-      className="w-24 h-12 mt-5 flex items-center justify-center shadow-xl"
+      className="flex items-center justify-center shadow-xl fixed right-6 bottom-6 rounded-full p-4 bg-blue-600 hover:bg-blue-500 cursor-pointer"
     >
-      <IconPencil className="text-default-600 text-2xl" />
-    </Button>
+      <IconPencil className="text-default-50" size={28} />
+    </div>
   );
 };
 
