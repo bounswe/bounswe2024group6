03e69728d28f33547c940e2ed3c086b6f304609d
Oktay Özel <mail.oktayozel@gmail.com>
2024-11-22 21:10:17 +0300

    aaahsdujnv

--- backend/app/views_directory/feed_views.py
+++ backend/app/views_directory/feed_views.py
@@ -4,12 +4,13 @@ from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework import status
+from app.models import Comment
+from app.serializers import CommentSerializer
 import random
 
 @api_view(['GET'])
 @permission_classes([IsAuthenticated])
 def get_user_post_feed(request):
-
     posts = Post.objects.all()
 
     if posts.exists():
@@ -25,7 +26,11 @@ def get_user_post_feed(request):
             "author": post.author,
             "created_at": post.created_at,
             "like_count": post.like_count,
-            "tags": [tag for tag in post.tags]
+            "tags": [tag for tag in post.tags],
+            "comments": CommentSerializer(
+                Comment.objects.filter(post=post, parent=None),  # Only top-level comments
+                many=True
+            ).data,
         }
         for post in random_posts
     ]
