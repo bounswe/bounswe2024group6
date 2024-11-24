c94e9c7ccd06e24287e2cd57c53798cbcbd4e31e
yunusemreozdemir <emreozdemir@mail.com>
2024-11-24 11:29:55 +0300

    feat: fetch activities

--- frontend/src/pages/notifications.tsx
+++ frontend/src/pages/notifications.tsx
@@ -1,7 +1,13 @@
+import { useEffect } from "react";
 import Navbar from "../components/common/navbar.tsx";
 import NotificationCard from "../components/notification/notification-card.tsx";
+import axios from "axios";
+import { BASE_URL } from "../lib/baseURL";
+import { AuthActions } from "../components/auth/utils.tsx";
 
 export default function Notifications() {
+  const { getToken } = AuthActions();
+  const token = getToken("access");
   const notifications = [
     {
       id: "1233",
@@ -19,6 +25,25 @@ export default function Notifications() {
       timePassed: "1d ago",
     },
   ];
+
+  useEffect(() => {
+    axios
+      .post(
+        `${BASE_URL}/user-activities-as-object/`,
+        {},
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      )
+      .then((response) => {
+        console.log(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
 
   return (
     <div className="flex flex-col items-center">
