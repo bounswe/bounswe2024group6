4cb89e1095db0a14ec026f17d4941e581dc2c510
Elifnurdeniz <elfnrdeniz@gmail.com>
2024-11-24 20:40:53 +0300

    feat(frontend): implemented popover user cards

--- frontend/src/components/common/user-card.tsx
+++ frontend/src/components/common/user-card.tsx
@@ -1,50 +1,111 @@
 import React from "react";
 import { Avatar, Button, Card, CardBody, CardFooter, CardHeader } from "@nextui-org/react";
+import { Suspense, useState, useEffect } from "react";
+import axios from "axios";
+import { BASE_URL } from "../../lib/baseURL";
+import { Profile, ProfileResponse } from "../../types";
+import { convertProfileResponseToProfile } from "./utils";
+import { AuthActions } from "../../components/auth/utils.tsx";
+import { useNavigate, useLocation } from "react-router-dom";
+import Cookies from "js-cookie";
 
-export const UserCard = () => {
+type Props = {
+    username: string;
+};
+
+export const UserCard = ({
+    username,
+}: Props) => {
     const [isFollowed, setIsFollowed] = React.useState(false);
+    const { getToken } = AuthActions();
+    const token = getToken("access");
+    const [profile, setProfile] = useState<Profile | null>(null);
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        if (username) {
+            axios
+                .get(`${BASE_URL}/profile/${username}/`, {
+                    headers: {
+                        Authorization: `Bearer ${token}`,
+                        "Content-Type": "application/json",
+                    },
+                })
+                .then((response) => {
+                    const data: ProfileResponse = response.data;
+                    console.log(data);
+                    const profile = convertProfileResponseToProfile(data);
+                    setIsFollowed(profile.is_followed);
+                    setProfile(profile);
+                })
+                .catch((error) => {
+                    console.log(error);
+                });
+        }
+    }, [username, token]);
+
+    const toggleFollow = () => {
+        axios
+          .post(
+            `${BASE_URL}/profile/${isFollowed ? "unfollow" : "follow"}/`,
+            {
+              username: profile?.username,
+            },
+            {
+              headers: {
+                "Content-Type": "application/json",
+                Authorization: `Bearer ${token}`,
+              },
+            }
+          )
+          .then((response) => {
+            console.log(response.data);
+          })
+          .catch((error) => {
+            console.log(error);
+          });
+        setIsFollowed(!isFollowed);
+      };
 
     return (
-        <Card shadow="none" className="max-w-[300px] border-none bg-transparent">
+        <Card shadow="none" className="w-[300px] border-none bg-transparent">
             <CardHeader className="justify-between">
                 <div className="flex gap-3">
-                    <Avatar isBordered radius="full" size="md" src="https://i.pravatar.cc/150?u=a04258114e29026702d" />
+                    <Avatar isBordered radius="full" size="md" src="https://nextui.org/avatars/avatar-1.png" />
                     <div className="flex flex-col items-start justify-center">
-                        <h4 className="text-small font-semibold leading-none text-default-600">Zoey Lang</h4>
-                        <h5 className="text-small tracking-tight text-default-500">@zoeylang</h5>
+                        <h4 className="text-small font-semibold leading-none text-default-600">{profile?.username}</h4>
+                        <h5 className="text-small tracking-tight text-default-500">@{profile?.level}</h5>
                     </div>
                 </div>
                 <div className="flex flex-row items-center gap-2">
-                    <Button
-                        className={isFollowed ? "bg-transparent text-foreground border-default-200" : ""}
-                        color="primary"
-                        radius="full"
-                        size="sm"
-                        variant={isFollowed ? "bordered" : "solid"}
-                        onPress={() => setIsFollowed(!isFollowed)}
-                    >
-                        {isFollowed ? "Unfollow" : "Follow"}
-                    </Button>
-                    <Button color="primary" variant="faded" size="sm" radius="full">
+                    {profile && profile.username !== Cookies.get("username") &&
+                        <Button
+                            className={isFollowed ? "bg-transparent text-foreground border-default-200" : ""}
+                            color="primary"
+                            radius="full"
+                            size="sm"
+                            variant={isFollowed ? "bordered" : "solid"}
+                            onClick={toggleFollow}
+                        >
+                            {isFollowed ? "Unfollow" : "Follow"}
+                        </Button>}
+                    <Button color="primary" variant="faded" size="sm" radius="full" onClick={() => navigate(`/profile/${username}`)}>
                         Profile
                     </Button>
                 </div>
             </CardHeader>
             <CardBody className="px-3 py-0">
                 <p className="text-small pl-px text-default-500">
-                    Full-stack developer, @getnextui lover she/her
-                    <span aria-label="confetti" role="img">
-                        🎉
-                    </span>
+                    {profile?.bio || "Hey, new learner here!"}
                 </p>
             </CardBody>
             <CardFooter className="gap-3">
                 <div className="flex gap-1">
-                    <p className="font-semibold text-default-600 text-small">4</p>
+                    <p className="font-semibold text-default-600 text-small">{profile?.following}</p>
                     <p className=" text-default-500 text-small">Following</p>
                 </div>
                 <div className="flex gap-1">
-                    <p className="font-semibold text-default-600 text-small">97.1K</p>
+                    <p className="font-semibold text-default-600 text-small">{profile?.followers}</p>
                     <p className="text-default-500 text-small">Followers</p>
                 </div>
             </CardFooter>
