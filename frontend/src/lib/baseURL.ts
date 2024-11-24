66736c6b9a2a0ff998aeeca68943ce03ae381dce
aoengin <ahmetoguzengin@hotmail.com>
2024-11-25 01:46:12 +0300

    change server IP

--- frontend/src/lib/baseURL.ts
+++ frontend/src/lib/baseURL.ts
@@ -1,4 +1,4 @@
-const BACKEND_URL: string = "http://3.70.214.28";
+const BACKEND_URL: string = "http://54.93.52.38";
 const BACKEND_PORT: string = "8000";
 
 export const BASE_URL: string = `${BACKEND_URL}:${BACKEND_PORT}`;
