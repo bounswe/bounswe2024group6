59db1eab39dd0869a58040850973e426feffcf36
yunusemreozdemir <emreozdemir@mail.com>
2024-12-14 16:49:19 +0300

    feat: configure protected routes to allow guest view

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -27,11 +27,7 @@ const router = createBrowserRouter([
   },
   {
     path: "/quizzes",
-    element: (
-      <ProtectedRoute>
-        <Quizzes />
-      </ProtectedRoute>
-    ),
+    element: <Quizzes />,
   },
   {
     path: "/quiz/:quizID",
@@ -43,11 +39,7 @@ const router = createBrowserRouter([
   },
   {
     path: "/quiz/:quizID/details",
-    element: (
-      <ProtectedRoute>
-        <QuizDetails />
-      </ProtectedRoute>
-    ),
+    element: <QuizDetails />,
   },
   {
     path: "/quiz/:quizResultID/review",
@@ -66,14 +58,6 @@ const router = createBrowserRouter([
     ),
   },
   {
-    path: "/quiz/:quizID/details",
-    element: (
-      <ProtectedRoute>
-        <QuizDetails />
-      </ProtectedRoute>
-    ),
-  },
-  {
     path: "/create-quiz",
     element: (
       <ProtectedRoute>
@@ -82,28 +66,12 @@ const router = createBrowserRouter([
     ),
   },
   {
-    path: "/quiz/:quizID/end",
-    element: (
-      <ProtectedRoute>
-        <QuizEnd />
-      </ProtectedRoute>
-    ),
-  },
-  {
     path: "/forum",
-    element: (
-      <ProtectedRoute>
-        <Forum />
-      </ProtectedRoute>
-    ),
+    element: <Forum />,
   },
   {
     path: "/post/:postID",
-    element: (
-      <ProtectedRoute>
-        <Post />
-      </ProtectedRoute>
-    ),
+    element: <Post />,
   },
   {
     path: "/compose-post",
@@ -115,11 +83,7 @@ const router = createBrowserRouter([
   },
   {
     path: "/profile/:username",
-    element: (
-      <ProtectedRoute>
-        <Profile />
-      </ProtectedRoute>
-    ),
+    element: <Profile />,
   },
   {
     path: "/profile/:username/edit",
@@ -131,11 +95,7 @@ const router = createBrowserRouter([
   },
   {
     path: "/browse",
-    element: (
-      <ProtectedRoute>
-        <Browse />
-      </ProtectedRoute>
-    ),
+    element: <Browse />,
   },
 ]);
 
@@ -148,4 +108,3 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
     </NextUIProvider>
   </React.StrictMode>
 );
-
